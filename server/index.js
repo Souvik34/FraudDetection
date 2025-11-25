98e2e58e47c2457097861fdb7bdb639f8133881d
Souvik <souvik13109@gmail.com>
2025-11-25 22:10:11 +0530

    File structure

---
+++ server/index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+const app = express();
+
+
+import dotenv from 'dotenv';
+dotenv.config(
+    {
+        path: './.env'
+    });
+const PORT = process.env.PORT || 4000;
+app.listen(PORT,() => {
+    console.log(`Server running on port ${PORT}`);
+})
+
+//DataBase Connection
+
+import connectDB from './src/config/db.js';
+connectDB();
